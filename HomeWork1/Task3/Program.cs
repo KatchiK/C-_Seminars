1afda1674b06eb1a6547448451d5abe089dc6e08
Aleksey Kachalov <a_kachalov@mail.ru>
2023-02-11 16:35:50 +0300

    Добавлена треться задача

--- HomeWork1/Task3/Program.cs
+++ HomeWork1/Task3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Clear();
+Console.WriteLine("Эта программа определяет, чётность введённого, Вами, числа.");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " является чётным числом.");
+}
+else
+{
+    Console.WriteLine("Число " + number + " является нечётным числом.");
+}
