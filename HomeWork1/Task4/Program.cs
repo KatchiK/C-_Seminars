f187c4163ddec939cfb6d56416681376618ec648
Aleksey Kachalov <a_kachalov@mail.ru>
2023-02-11 17:22:50 +0300

    Сделана 4-я задача

--- HomeWork1/Task4/Program.cs
+++ HomeWork1/Task4/Program.cs
@@ -5,3 +5,23 @@
 8 -> 2, 4, 6, 8
 */
 
+Console.Clear();
+Console.WriteLine("Эта программа покажет все чётные числа от 1 до указанного числа.");
+Console.WriteLine("Введите число от 2 до ~: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+
+if (N < 2)
+{
+    Console.WriteLine("Число " + N + " не принадлежит множеству чисел от 2 до ~.");
+}
+else
+{
+    Console.WriteLine("Четные числа от 1 до " + N + ":");
+    while (number <= N)
+    {
+        Console.Write(number + " ");
+        number = number + 2;
+    }
+}
+
