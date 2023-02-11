10bf4b9ec86430f2edd9fd9d8dc53afa5b628a5a
Aleksey Kachalov <a_kachalov@mail.ru>
2023-02-11 15:56:37 +0300

    Визуально отредактировал код

--- HomeWork1/Task2/Program.cs
+++ HomeWork1/Task2/Program.cs
@@ -7,24 +7,24 @@
 
 Console.Clear();
 int count = 1;
-Console.WriteLine ("Эта программа определяет, какое из введённых Вами чисел самое большое.");
+Console.WriteLine("Эта программа определяет, какое из введённых Вами чисел самое большое.");
 
-Console.WriteLine ("Укажите сколько чисел будет введено:");
+Console.WriteLine("Укажите сколько чисел будет введено:");
 int quantity = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите 1-е число: ");
 int maxNumber = Convert.ToInt32(Console.ReadLine());
 
- while (count < quantity)
- {
-    count ++;
+while (count < quantity)
+{
+    count++;
     Console.Write("Введите " + count + "-е число: ");
     int number = Convert.ToInt32(Console.ReadLine());
     if (maxNumber < number)
     {
         maxNumber = number;
     }
- }
+}
 
- Console.WriteLine("Самое большое число, из чисел введённых Вами, это число " + maxNumber + ".");
+Console.WriteLine("Самое большое число, из чисел введённых Вами, это число " + maxNumber + ".");
 
