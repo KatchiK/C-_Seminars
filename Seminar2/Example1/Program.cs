bc830e568db57a10a42318cb676ab2d13a2e9e0b
KatchiK <a_kachalov@mail.ru>
2023-02-14 20:50:35 +0300

    Добавлена 1-я задача

---
+++ Seminar2/Example1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = new Random().Next(10, 100);
+int firstValue = number/10;
+int secondValue = number%10;
+int max = firstValue;
+if (max < secondValue)
+{
+  max = secondValue;
+}
+Console.WriteLine("Максимальная цифра в числе " + number + " это" + max);
