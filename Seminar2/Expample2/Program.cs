63a60fda2cba3075f73bd8dc14cc05abb876380e
KatchiK <a_kachalov@mail.ru>
2023-02-14 21:11:49 +0300

    Добавлена 2-я задача

---
+++ Seminar2/Expample2/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = new Random().Next(100, 1000);
+int result = (number/100)*10 + number%10;
+Console.WriteLine("Исходное число " + number + "; 1 и 3 цифры - " + result);
