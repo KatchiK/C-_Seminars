41a06777ed6732116545737645357a616ffac99e
KatchiK <a_kachalov@mail.ru>
2023-02-15 21:19:59 +0300

    Окончательное решение 1-ой задачи

--- HomeWork2/Task1(10)/Program.cs
+++ HomeWork2/Task1(10)/Program.cs
@@ -6,12 +6,13 @@
 Console.WriteLine("Эта программа принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
 Console.WriteLine("Введите трёхзначное число:");
 int number = Convert.ToInt32(Console.ReadLine());
-if ((99 < number && number < 1000) || (-1000 < number && number < -99))
-{
-  int secondDigit = Math.Abs((number % 100)/10);
-  Console.WriteLine(secondDigit);
-}
-else
+
+while (!((99 < number && number < 1000) || (-1000 < number && number < -99)))
 {
   Console.WriteLine("Вы ввели не трёхзначное число!!!");
+  Console.WriteLine("Введите трёхзначное число ещё раз:");
+ number = Convert.ToInt32(Console.ReadLine());
 }
+
+  int secondDigit = Math.Abs((number % 100)/10);
+  Console.WriteLine($"Вторая цифра в числе {number} это цифра {secondDigit}");
