e14e54203e561a9bc160b2fb9ddf88cd3ef4909c
KatchiK <a_kachalov@mail.ru>
2023-02-15 20:23:32 +0300

    Самое простое решение 1-й задачи

---
+++ HomeWork2/Task1(10)/Program.cs
@@ -0,0 +1,17 @@
+﻿ /*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Эта программа принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((99 < number && number < 1000) || (-1000 < number && number < -99))
+{
+  int secondDigit = Math.Abs((number % 100)/10);
+  Console.WriteLine(secondDigit);
+}
+else
+{
+  Console.WriteLine("Вы ввели не трёхзначное число!!!");
+}
