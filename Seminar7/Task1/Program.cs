346c5156764a714556b4a26d4ea98873c1dc10aa
Aleksey Kachalov <a_kachalov@mail.ru>
2023-03-03 23:31:19 +0300

    add seminar7

---
+++ Seminar7/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+*/
+
+int[,] GetMatrix(int rows, int cols, int minValue, int maxValue)
+{
+    int[,] matrix = new int[rows, cols]; // [строчка, столбец]
+    for (int i = 0; i < rows; i++) // строчки; rows = matrix.GetLength(0)
+    {
+        // i,j,m,k - индексы
+        for (int j = 0; j < cols; j++)// столбцы; cols = matrix.GetLength(1)
+        {
+            matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix; // Вернули заполненную табличку
+}
+
+void PrintMatrix(int[,] matr) // Печатаем двумерный массив
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // проход по строчкам
+    {
+        for (int m = 0; m < matr.GetLength(1); m++) // Проход по столбцам
+        {
+            Console.Write(matr[i, m] + "\t"); // "\t" = 4 пробела или tab
+        }
+        Console.WriteLine();
+    }
+}
+
+// 1. Создать массив
+int[,] resultMatrix = GetMatrix(7,8,-10,10);
+// 3 строчки и 4 столбца, числа от 0 до 10 включительно
+
+// 2. Посмотрим на нашу матрицу
+PrintMatrix(resultMatrix);
---
+++ Seminar7/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 48: 
+Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: 
+Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+int[,] GetMatrix(int rows, int cols)
+{
+    int[,] matrix = new int[rows, cols]; // [строчка, столбец]
+    for (int i = 0; i < rows; i++) // строчки; rows = matrix.GetLength(0)
+    {
+        // i,j,m,k - индексы
+        for (int j = 0; j < cols; j++)// столбцы; cols = matrix.GetLength(1)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix; // Вернули заполненную табличку
+}
+
+void PrintMatrix(int[,] matr) // Печатаем двумерный массив
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // проход по строчкам
+    {
+        for (int m = 0; m < matr.GetLength(1); m++) // Проход по столбцам
+        {
+            Console.Write(matr[i, m] + "\t"); // "\t" = 4 пробела или tab
+        }
+        Console.WriteLine();
+    }
+}
+
+// 1. Создать массив
+int[,] resultMatrix = GetMatrix(3, 4);
+// 3 строчки и 4 столбца, числа от 0 до 10 включительно
+
+// 2. Посмотрим на нашу матрицу
+PrintMatrix(resultMatrix);
