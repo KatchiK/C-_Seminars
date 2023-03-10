feb1826bfa6a54532fa09852f2ba0c73cb0fd2d8
Aleksey Kachalov <a_kachalov@mail.ru>
2023-03-10 17:17:32 +0300

    work, curve checkф on щтщт identity

--- HomeWork8/Task4_60/Program.cs
+++ HomeWork8/Task4_60/Program.cs
@@ -6,63 +6,60 @@
 26(1,0,1) 55(1,1,1)
 */
 
-int[,,] GetMatrix(int axisX, int axisY, int axisZ, int minValue, int maxValue) // Создание трёхмерного массива с рандомными числами.
+int[,,] GetArray3d(int axisX, int axisY, int axisZ, int minValue, int maxValue) // Создание трёхмерного массива с рандомными числами.
 {
-  int[,,] arr3d = new int[axisX, axisY, axisZ];
-  for (int x = 0; x < axisX; x++)
-  {
-    for (int y = 0; y < axisY; y++)
+    int[,,] arr3d = new int[axisX, axisY, axisZ];
+    for (int x = 0; x < axisX; x++)
     {
-      for (int z = 0; z < axisZ; z++)
-      {
-      NewRandom:
-        arr3d[x, y, z] = new Random().Next(minValue, maxValue + 1);
-
-        if (x != 0 && y != 0 && z != 0)
+        for (int y = 0; y < axisY; y++)
         {
-          for (int x1 = 0; x1 < x; x1++)// Проверка на повтор числа.
-          {
-            Console.Write(x1);
-            for (int y1 = 0; y1 < y; y1++)
+            for (int z = 0; z < axisZ; z++)
             {
-              Console.Write(y1);
-              for (int z1 = 0; z1 < z; z1++)
-              {
-                Console.Write(z1 + "\t");
-                if (arr3d[x, y, z] == arr3d[x1, y1, z1])
+            NewRandom:
+                arr3d[x, y, z] = new Random().Next(minValue, maxValue + 1);
+                for (int x1 = 0; x1 < axisX; x1++)// Проверка на повтор числа.
                 {
-                  Console.Write($"{arr3d[x, y, z]} ");
-                  goto NewRandom;
+                    for (int y1 = 0; y1 < axisY; y1++)
+                    {
+                        for (int z1 = 0; z1 < axisZ; z1++)
+                        {
+                            if (x == x1 && y == y1 && z == z1)
+                            {
+                                goto Next;
+                            }
+                            else
+                            {
+                                if (arr3d[x1, y1, z1] == (arr3d[x, y, z]))
+                                {
+                                    goto NewRandom;
+                                }
+                            }
+                        Next:;
+                        }
+                    }
                 }
-              }
             }
-          }
         }
-
-
-      }
-
     }
-  }
-  Console.WriteLine();
-  return arr3d;
+    return arr3d;
 }
 
-void PrintMatrix(int[,,] arr3d) // Печатаем 3 массив
+void PrintMatrix(int[,,] arr3d) // Печатаем трехмерный массив
 {
-  for (int x = 0; x < arr3d.GetLength(0); x++)
-  {
-    for (int y = 0; y < arr3d.GetLength(1); y++)
+  Console.WriteLine();
+    for (int x = 0; x < arr3d.GetLength(0); x++)
     {
-      for (int z = 0; z < arr3d.GetLength(2); z++)
-      {
-        Console.Write($"{arr3d[x, y, z]}({x},{y},{z})\t");
-      }
-      Console.WriteLine();
+        for (int y = 0; y < arr3d.GetLength(1); y++)
+        {
+            for (int z = 0; z < arr3d.GetLength(2); z++)
+            {
+                Console.Write($"{arr3d[x, y, z]}({x},{y},{z})\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-  }
 }
 
-int[,,] array3d = GetMatrix(3, 3, 3, 0, 100);
+int[,,] array3d = GetArray3d(3, 3, 3, 0, 27);
 PrintMatrix(array3d);
